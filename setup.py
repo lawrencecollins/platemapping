5da6fc464cff1dad14c1362e7e6ee9d779169046
lawrencecollins <67695762+lawrencecollins@users.noreply.github.com>
2020-08-20 13:02:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="platemapping", 
-    version="0.0.2",
+    version="1.0.0",
     author="Stuart Warriner, Lawrence Collins",
     author_email="s.l.warriner@leeds.ac.uk, lawrencejordancollins@gmail.com",
     description="Plate map uploading, processing & visualisaion",
